40f9cf3b7eab204f767f01a415be64890636b9b6
Zain Ul Abideen <zainjaveed97@gmail.com>
2022-07-30 15:20:59 +0500

    Mininum Max Sum Solution Added

---
+++ Python/miniMaxSum.py
@@ -0,0 +1,22 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    total = sum(arr)
+    print(total - max(arr), total - min(arr))
+
+if __name__ == '__main__':
+
+    # arr = list(map(int, input().rstrip().split()))
+    arr = [1, 2, 3, 4, 5]
+
+    miniMaxSum(arr)
