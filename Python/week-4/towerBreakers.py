05de74b51d8dbc21834dbcc5fe6a77357bbe0fe2
Zain Ul Abideen <zainjaveed97@gmail.com>
2022-08-06 23:13:41 +0500

    Tower Breakers Solution Added

---
+++ Python/week-4/towerBreakers.py
@@ -0,0 +1,28 @@
+#
+# Complete the 'towerBreakers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def towerBreakers(n, m):
+    # Write your code here
+    return 2 if n % 2 == 0 or m == 1 else 1
+
+
+if __name__ == '__main__':
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        result = towerBreakers(n, m)
+    
+        print(f'Result: {result}')
+
