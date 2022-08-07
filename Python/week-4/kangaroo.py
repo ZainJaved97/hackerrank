7d9ce227e20a0bd767b6fc43e03b8b5532856b51
Zain Ul Abideen <zainjaveed97@gmail.com>
2022-08-07 23:57:44 +0500

    Number Line Jumps Solution Added

---
+++ Python/week-4/kangaroo.py
@@ -0,0 +1,33 @@
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    # Write your code here
+    if v2 == v1:
+        return 'NO'
+
+    ratio = (x1 - x2) / (v2 - v1)
+    return 'YES' if ratio >= 0 and ratio.is_integer() else 'NO'
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    print(f'Result: {result}')
